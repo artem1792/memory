3ef9916f45426f68a27ce7c7ab97818d12eb0e8d
Студенческая <@rbiu.local>
2025-09-23 11:14:32 +0500

    layout

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-console.log("Hello!");
+const cards = document.querySelectorAll(".card");
+for (let card of cards) {
+    card.addEventListener("click", () => {
+        card.classList.add("card-show");
+    })
+}
