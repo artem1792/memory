12fc8dbf24c39f1e3f6ea433fbcb64793e8f3d8f
Студенческая <@rbiu.local>
2025-09-30 10:53:43 +0500

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-const cards = document.querySelectorAll(".card");
-for (let card of cards) {
+import { images } from "./data.js";
+
+const container = document.querySelector(".container");
+images.forEach((item)=>{
+    const card = document.createElement("div");
+    card.classList.add("card");
+    const img = document.createElement("img");
+    img.src = item.url;
+    img.alt = "";
+    card.append(img);
+    container.append(card);
     card.addEventListener("click", () => {
-        card.classList.add("card-show");
+        card.classList.toggle("card-show");
+        setTimeout(() => {
+            card.classList.toggle("card-show");
+        }, 1000);
     })
-}
+});
+
+// const cards = document.querySelectorAll(".card");
+// for (let card of cards) {
+//     card.addEventListener("click", () => {
+//         card.classList.toggle("card-show");
+//         setTimeout(() => {
+//             card.classList.toggle("card-show");
+//         }, 1000);
+//     })
+// }
